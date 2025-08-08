bd1a9887cf27036cf11dc82ec8c3c9ff99edb7f2
DEVASANJAY001 <devasanjaynatarajan@gmail.com>
2025-08-08 11:50:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/Categories" element={<Categories />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/api-docs" element={<ApiDocs />} />
